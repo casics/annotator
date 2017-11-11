9dc42a907dce69ee64567f77f8188cd34300f68d
Michael Hucka <mhucka@caltech.edu>
2017-11-11 11:06:41 -0800

    Add the usual module files

---
+++ annotator/__init__.py
@@ -0,0 +1,10 @@
+# =============================================================================
+# @file    __init__.py
+# @brief   CASICS Annotator package __init__ file
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/casics/annotator
+# =============================================================================
+
+from .annotator import *
+from .version import *
